d654f4c11833a37b7ebae0aa542952a9d4e0fd41
Jeffrey Seneff <jeffsnff@gmail.com>
2024-01-06 13:50:41 -0700

    finished Installing Cypress and Writing your first test lesson

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,6 +1,19 @@
 describe('home page test', () => {
-  it('H1 contains correct text', () => {
+  beforeEach(() => {
     cy.visit('http://localhost:3000')
-    cy.get('h1').contains('Testing Next.js Applications with Cypress')
+  })
+
+
+  it('H1 contains correct text', () => {
+    cy
+      .get('[data-test="hero-heading"]')
+      .should('be.visible')
+      .contains('Testing Next.js Applications with Cypress')
+  })
+
+  it('Description bullets are present', () => {
+    cy.get('dt').eq(0).contains('4 Courses')
+    cy.get('dt').eq(1).contains('25+ Lessons')
+    cy.get('dt').eq(2).contains('Free and Open Source')
   })
 })
